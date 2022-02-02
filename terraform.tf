4b8034ca9f44ad8d1855ab61bd684a039c604f55
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2022-02-02 15:22:50 +0530

    updated

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,6 @@ cidr_block = "10.0.0.0/16"
 enable_dns_support = "true"
 enable_dns_hostnames = "true"
 }
-
 resource "aws_subnet" "private-subnet1" {
 vpc_id = "${aws_vpc.main.id}"
 cidr_block = "10.0.1.0/24"
